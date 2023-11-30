058a4d8becb2b6f05b989c2e3c9926fc9abb0033
Prayush Adhikari <115662438+adhikareeprayush@users.noreply.github.com>
2023-11-30 22:11:38 +0545

    Update chatbot.php

--- chatbot.php
+++ chatbot.php
@@ -1,29 +1,36 @@
 <?php
 
+// Include the database configuration file
 include('db_config.php');
 
-
-
-
+// Check if the 'message' parameter is set in the GET request
 if (isset($_GET['message'])) {
+    // Retrieve the user's message from the GET request
     $message = $_GET['message'];
 
+    // Prepare a SQL statement to retrieve the response from the database
     $stmt = $conn->prepare("SELECT response FROM messages_chatbot WHERE user_msg=? LIMIT 1");
 
+    // Bind the user's message as a parameter to the SQL statement
     $stmt->bind_param('s', $message);
 
-    // execute query
+    // Execute the SQL query
     if ($stmt->execute()) {
+        // Bind the result of the query to a variable
         $stmt->bind_result($response_message);
 
+        // Store the result locally
         $stmt->store_result();
 
+        // Check if exactly one row is returned
         if ($stmt->num_rows() == 1) {
+            // Fetch the result and create an associative array
             $stmt->fetch();
             $result = ['response_message' => $response_message];
+            // Encode the result as JSON and send it to the client
             echo json_encode($result);
         } else {
-            // Provide a default response if num_rows is not equal to 1
+            // Provide a default response if the number of rows is not equal to 1
             echo json_encode(['response_message' => 'Sorry, I don\'t have a response for that.']);
         }
     } else {
@@ -31,6 +38,8 @@ if (isset($_GET['message'])) {
         echo json_encode(['response_message' => 'Sorry, I encountered an error while processing your request.']);
     }
 } else {
+    // Respond with a default message if the 'message' parameter is not set
     echo json_encode(['response_message' => 'Sorry, I don\'t understand you.']);
 }
+
 ?>
